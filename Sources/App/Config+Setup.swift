8145df8a3df802e93fad1ae9d6dbc025e5f1b66c
Daniel Hour <daniel.hour@me.com>
2017-08-03 21:41:05 -0700

    Config+Setup.swift: minor refactor

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -8,28 +8,26 @@ extension Config {
         // allow fuzzy conversions for these types (add your own types here)
         Node.fuzzy = [Row.self, JSON.self, Node.self]
 
+        try setupMiddleware()
+        try setupModelSchemas()
         try setupProviders()
-        try setupPreparations()
     }
     
-    /**
-     Configure providers
-     */
-    private func setupProviders() throws {
-        try self.addProvider(FluentProvider.Provider.self)
-        try self.addProvider(PostgreSQLProvider.Provider.self)
     private func setupMiddleware() throws {
         self.addConfigurable(middleware: RequestVerificationMiddleware(), name: "request-verification")
     }
     
-    /**
-     Add all models that should have their schemas prepared before the app boots
-     */
-    private func setupPreparations() throws {
+    private func setupModelSchemas() throws {
         self.preparations.append(Post.self)
         self.preparations.append(Pet.self)
         self.preparations.append(PullQuote.self)
         self.preparations.append(Tag.self)
         self.preparations.append(Pivot<PullQuote, Tag>.self)
     }
+    
+    private func setupProviders() throws {
+        try self.addProvider(FluentProvider.Provider.self)
+        try self.addProvider(PostgreSQLProvider.Provider.self)
+    }
+    
 }
