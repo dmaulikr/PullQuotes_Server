61d1e178abbea85a162946417facf616380f9f01
Daniel Hour <daniel.hour@me.com>
2017-08-06 16:17:22 -0700

    Config+Setup.swift: naming refactor

--- Sources/App/Config+Setup.swift
+++ Sources/App/Config+Setup.swift
@@ -12,21 +12,27 @@ import PostgreSQLProvider
 
 extension Config {
     
+    //---------------------------------------------------------------------------------------
+    //MARK: - Public Methods
+    
     public func setup() throws {
         
         // allow fuzzy conversions for these types (add your own types here)
         Node.fuzzy = [Row.self, JSON.self, Node.self]
 
-        try setupMiddleware()
-        try setupProviders()
-        try setupSchemas()
+        try configureMiddleware()
+        try configureProviders()
+        try configureSchemas()
     }
     
-    private func setupMiddleware() throws {
+    //---------------------------------------------------------------------------------------
+    //MARK: - Private Methods
+    
+    private func configureMiddleware() throws {
         self.addConfigurable(middleware: RedirectMiddleware.login(), name: "redirect")
     }
     
-    private func setupSchemas() throws {
+    private func configureSchemas() throws {
         self.preparations.append(User.self)
         self.preparations.append(Token.self)
         self.preparations.append(PullQuote.self)
@@ -34,7 +40,7 @@ extension Config {
         self.preparations.append(Pivot<PullQuote, Tag>.self)
     }
     
-    private func setupProviders() throws {
+    private func configureProviders() throws {
         try self.addProvider(FluentProvider.Provider.self)
         try self.addProvider(PostgreSQLProvider.Provider.self)
     }
